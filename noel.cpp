a93a32f541c9c7862e6bcb44961415362573bd91
Anderson Garrote <ander.user@gmail.com>
2018-05-29 19:10:22 -0300

    Adicionado noel.cpp

---
+++ noel.cpp
@@ -0,0 +1,17 @@
+/*!
+ * [maracas description]
+ * @param n [description]
+ * @param c [description]
+ */
+#include <iostream>
+int main()
+{
+
+    int grunido=-1;
+    if (grunido<0) {
+        std::cout << "JOOJ" << '\n';
+    }
+    for (int i = 0; i < 2; i++) {
+        int Kek = 0;
+    }
+}
